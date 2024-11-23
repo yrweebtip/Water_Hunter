7de144b64c1df67daaea4ab454b3e89e206936eb
Devan <devankhan360@gmail.com>
2024-11-23 13:59:59 +0700

    Movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,102 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float walkSpeed = 3f;
-    public float crouchSpeed = 1.5f;
-    public float jumpForce = 5f;
-    public float crouchHeight = 0.5f;
-    public float standHeight = 2f;
-    public Transform playerCamera;
-    public float rotationSpeed = 2f;
-    private bool isCrouching = false;
-    private CapsuleCollider playerCollider;
-    private Vector3 originalCenter;
+    public CharacterController characterController;
+    public float movementSpeed = 5.0f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundLayer;
+    public float jumpHeight = 3.0f;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    public Transform playerCamera; // Referensi ke kamera
+    public float crouchHeight = 1f; // Tinggi saat jongkok
+    public float standHeight = 2f; // Tinggi normal
+    public float crouchSpeed = 2f; // Kecepatan saat jongkok
+    public float normalSpeed = 5f; // Kecepatan normal
 
     private float currentSpeed;
-    private Rigidbody rb;
-
-    private bool isGrounded;  // Menambahkan variabel untuk memeriksa apakah karakter berada di tanah
-    public float groundCheckDistance = 0.1f;  // Jarak untuk memeriksa tanah
-    public LayerMask groundLayer;  // Layer tanah
+    private bool isCrouching = false;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        playerCollider = GetComponent<CapsuleCollider>();
-        originalCenter = playerCollider.center; // Menyimpan posisi center collider awal
-        currentSpeed = walkSpeed; // Set default speed ke walkSpeed
+        if (characterController == null)
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+
+        currentSpeed = normalSpeed;
     }
 
     void Update()
     {
-        CheckGrounded();  // Memeriksa apakah pemain berada di tanah
-        Move();
-        Jump();
-        Crouch();
-        Rotate();
-    }
+        // Cek apakah player di tanah
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
 
-    void Move()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        transform.Translate(move * currentSpeed * Time.deltaTime, Space.World);
-    }
-
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && isGrounded) // Melompat hanya jika berada di tanah
+        if (isGrounded && velocity.y < 0)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            velocity.y = -2f; // Reset kecepatan vertikal saat menyentuh tanah
+        }
+
+        HandleMovement();
+        HandleCrouch();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Terapkan gravitasi
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        characterController.Move(move * currentSpeed * Time.deltaTime);
+    }
+
+    void HandleCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl)) // Tombol untuk mulai jongkok
+        {
+            ToggleCrouch();
         }
     }
 
-    void Crouch()
+    void ToggleCrouch()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl) && !isCrouching)
+        isCrouching = !isCrouching;
+
+        if (isCrouching)
         {
-            // Jongkok
-            isCrouching = true;
-            currentSpeed = crouchSpeed; // Mengubah kecepatan saat jongkok
-            playerCollider.height = crouchHeight; // Menurunkan tinggi collider
-            playerCollider.center = new Vector3(0, crouchHeight / 2, 0); // Mengatur posisi center collider
-            playerCamera.localPosition = new Vector3(0, crouchHeight / 2, 0); // Menurunkan kamera agar tidak terlalu tinggi
+            // Kurangi tinggi karakter dan kamera
+            characterController.height = crouchHeight;
+            playerCamera.localPosition = new Vector3(0, crouchHeight / 2, 0);
+            currentSpeed = crouchSpeed;
         }
-        else if (Input.GetKeyDown(KeyCode.LeftControl) && isCrouching)
+        else
         {
-            // Berdiri
-            isCrouching = false;
-            currentSpeed = walkSpeed; // Mengembalikan kecepatan normal
-            playerCollider.height = standHeight; // Mengembalikan tinggi collider
-            playerCollider.center = originalCenter; // Mengembalikan posisi center collider
-            playerCamera.localPosition = new Vector3(0, standHeight / 2, 0); // Mengembalikan posisi kamera
+            // Kembali ke tinggi normal
+            characterController.height = standHeight;
+            playerCamera.localPosition = new Vector3(0, standHeight / 2, 0);
+            currentSpeed = normalSpeed;
         }
-    }
-
-    void Rotate()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
-
-        transform.Rotate(Vector3.up * mouseX); // Rotasi horizontal
-
-        // Rotasi vertikal
-        float rotationX = playerCamera.localEulerAngles.x - mouseY;
-        rotationX = Mathf.Clamp(rotationX, -90f, 90f); // Mencegah rotasi melebihi batas
-        playerCamera.localEulerAngles = new Vector3(rotationX, 0, 0);
-    }
-
-    // Fungsi untuk memeriksa apakah pemain berada di tanah
-    void CheckGrounded()
-    {
-        // Menggunakan raycast untuk memeriksa apakah karakter berada di tanah
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerCollider.height / 2 + groundCheckDistance, groundLayer);
     }
 }
-
-
---
+++ Assets/Testing/movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public CharacterController controller; 
+    public float movementSpeed = 5.0f; 
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+    public float jumpForce = 5f;       // Kekuatan lompatan
+    public bool isGrounded = false;    // Status apakah pemain berada di tanah atau tidak
+    public LayerMask groundLayer;      // Layer untuk tanah
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    { 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical"); 
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical; 
+        velocity.y += gravity * Time.deltaTime; 
+
+        controller.Move(move * movementSpeed * Time.deltaTime); 
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;  // Mengatur status pemain agar tidak bisa lompat lagi sampai menyentuh tanah
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Mengecek apakah pemain menyentuh tanah
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
