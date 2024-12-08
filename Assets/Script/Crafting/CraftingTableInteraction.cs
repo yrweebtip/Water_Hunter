8cb0fd961ba8e074e55fd8ad895e97c3480a7592
yrweebtip <mlatifdiaz@gmail.com>
2024-12-08 07:49:10 +0700

    Benerin Panel

--- Assets/Script/Crafting/CraftingTableInteraction.cs
+++ Assets/Script/Crafting/CraftingTableInteraction.cs
@@ -3,46 +3,46 @@ using UnityEngine.SceneManagement;
 
 public class CraftingTableInteraction : MonoBehaviour
 {
-    public string craftingSceneName; // Nama scene untuk crafting
-    public string afterscene1 = "Scene1.2"; // Scene berikutnya jika asalnya dari Scene1
-    public string afterscene2 = "Scene2.2"; // Scene berikutnya jika asalnya dari Scene2
+    public string craftingSceneName; 
+    public string afterscene1 = "Scene1.2"; 
+    public string afterscene2 = "Scene2.2"; 
     public string afterscene3 = "Scene3.3";
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && AllItemsCollected())
         {
-            // Simpan nama scene asal dan tujuan
+            
             string currentSceneName = SceneManager.GetActiveScene().name;
-            PlayerPrefs.SetString("LastScene", currentSceneName); // Nama scene asal
-            PlayerPrefs.SetString("NextScene", GetNextScene(currentSceneName)); // Nama scene tujuan
+            PlayerPrefs.SetString("LastScene", currentSceneName); 
+            PlayerPrefs.SetString("NextScene", GetNextScene(currentSceneName)); 
             PlayerPrefs.Save();
 
-            // Pindah ke CraftingScene
+           
             SceneManager.LoadScene(craftingSceneName);
         }
     }
 
     private string GetNextScene(string currentScene)
     {
-        // Tentukan scene tujuan berdasarkan asal scene
+        
         if (currentScene == "Level 1")
         {
-            return afterscene1; // Jika asal dari Scene1
+            return afterscene1; 
         }
         else if (currentScene == "Level 2")
         {
-            return afterscene2; // Jika asal dari Scene2
+            return afterscene2; 
         }
         else if (currentScene == "Level 3")
         {
-            return afterscene3; // Jika asal dari Scene2
+            return afterscene3; 
         }
-        return ""; // Default jika nama scene tidak dikenali
+        return "";
     }
 
     private bool AllItemsCollected()
     {
-        return CollectItem.hasKerikil && CollectItem.hasArang && CollectItem.hasSpons && CollectItem.hasIjuk;
+        return CollectItem.hasKerikil && CollectItem.hasArang && CollectItem.hasSpons && CollectItem.hasIjuk && CollectItem.hasBotol;
     }
 }
 
